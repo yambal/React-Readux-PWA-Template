68a982a82122d961793178bfc9f196ad5c6b3b2a
June YAMAMOTO <june@yambal.net>
2020-06-09 20:48:26 +0900

    Updates

---
+++ precache-manifest.aebeff98722afd58ef856da92b32f4bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5453573a18231ac86cafe31cd74aaf91",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7c9a96e1aefc96568a05",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f31afe55779c1ba321ab",
+    "url": "./static/js/2.2a64e31d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2a64e31d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c9a96e1aefc96568a05",
+    "url": "./static/js/main.d944d896.chunk.js"
+  },
+  {
+    "revision": "33af829b2126bbebb255",
+    "url": "./static/js/runtime-main.9e7c238e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
